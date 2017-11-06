e63ac2689446625e798afe78e6f37efbf88197ca
alaurentinoofficial <alaurentino.br@gmail.com>
2017-11-06 00:34:25 -0200

    Add SerialPort Script

---
+++ Assets/Scripts/SerialPortComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class SerialPortComponent : MonoBehaviour
+{
+	SerialPort serial = new SerialPort("COM7", 9600);
+
+	void Start ()
+	{
+		serial.Open ();
+	}
+
+	void Update()
+	{
+		if (serial.IsOpen) {
+			serial.WriteLine (NetworkComponent.Camera.rotation.y.ToString ());
+			Debug.Log (serial.ReadLine());
+		}
+	}
+}
